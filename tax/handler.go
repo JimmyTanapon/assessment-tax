2a83b59c9b32b183e4a4136391316f809caa29c2
Jimmmy <paisuwan16@gmailcom>
2024-04-24 00:43:47 +0700

    feat(init): initial commit  new feature calulateTax use WTH to calulate discount

--- tax/handler.go
+++ tax/handler.go
@@ -1,7 +1,6 @@
 package tax
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -42,44 +41,8 @@ func (h *Handler) TaxHandler(c echo.Context) error {
 	if err := c.Bind(&incomeDetails); err != nil {
 		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
 	}
-	h.taxReduction()
-	taxAmount := calculateTax(incomeDetails.TotalIncome)
+
+	taxAmount := h.CalculateTax(incomeDetails)
 	return c.JSON(http.StatusOK, TaxResponse{Tax: taxAmount})
-
-}
-func calculateTax(income float64) float64 {
-
-	taxReduction := 60000.0
-	taxableIncome := income - taxReduction
-	var taxAmount float64
-	taxLevelindex := 0
-	maxIncomLevel := *TaxLevel[len(TaxLevel)-1].MaxIncome
-
-	for index, tax := range TaxLevel {
-
-		if index > len(TaxLevel) {
-			continue
-		}
-		adjustindex := index - 1
-		if adjustindex < 0 {
-			adjustindex = 0
-		}
-		if taxableIncome < *TaxLevel[adjustindex].MaxIncome {
-			continue
-		}
-		taxLevelindex += 1
-		taxAmount += tax.CalculateTaxRate(taxableIncome, *TaxLevel[adjustindex].MaxIncome, maxIncomLevel)
-	}
-	log.Println("taxlevle", taxLevelindex)
-
-	return taxAmount
-}
-
-func (h *Handler) taxReduction() {
-	discount, err := h.store.Discounts()
-	if err != nil {
-		log.Fatal("err!", err)
-	}
-	log.Println(discount)
 
 }
--- tax/tax.go
+++ tax/tax.go
@@ -1,6 +1,9 @@
 package tax
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type TaxDiscount struct {
 	ID                 int       `json:"id"`
@@ -9,6 +12,11 @@ type TaxDiscount struct {
 	Min_discount_value float64   `json:"min_discount_value"`
 	Max_discount_value float64   `json:"max_discount_value"`
 	CreatedAt          time.Time `json:"created_at"`
+}
+type TaxDiscountType struct {
+	PersonalDeduction TaxDiscount
+	Donation          TaxDiscount
+	Kreceipt          TaxDiscount
 }
 type Allowance struct {
 	AllowanceType string  `json:"allowanceType"`
@@ -19,6 +27,61 @@ type IncomeDetails struct {
 	TotalIncome float64     `json:"totalIncome"`
 	WHT         float64     `json:"wht"`
 	Allowances  []Allowance `json:"allowances"`
+}
+
+func (h *Handler) taxReduction() TaxDiscountType {
+	discounts, err := h.store.Discounts()
+	var personalDeduction, donation, kreceipt TaxDiscount
+
+	if err != nil {
+		log.Fatal("err!", err)
+	}
+	for _, discount := range discounts {
+		switch discount.Discount_Type {
+		case "personalDeduction":
+			personalDeduction = discount
+		case "donation":
+			donation = discount
+		case "k-receipt":
+			kreceipt = discount
+		default:
+			log.Fatal("err!", discount)
+		}
+	}
+	var typeofTaxDicounst = TaxDiscountType{
+		PersonalDeduction: personalDeduction,
+		Donation:          donation,
+		Kreceipt:          kreceipt,
+	}
+
+	return typeofTaxDicounst
+}
+
+func (h *Handler) CalculateTax(income IncomeDetails) float64 {
+	taxType := h.taxReduction()
+	taxableIncome := income.TotalIncome - taxType.PersonalDeduction.Discount_value
+	var taxAmount float64
+	taxLevelindex := 0
+	maxIncomLevel := *TaxLevel[len(TaxLevel)-1].MaxIncome
+
+	for index, tax := range TaxLevel {
+
+		if index > len(TaxLevel) {
+			continue
+		}
+		adjustindex := index - 1
+		if adjustindex < 0 {
+			adjustindex = 0
+		}
+		if taxableIncome < *TaxLevel[adjustindex].MaxIncome {
+			continue
+		}
+		taxLevelindex += 1
+		taxAmount += tax.CalculateTaxRate(taxableIncome, *TaxLevel[adjustindex].MaxIncome, maxIncomLevel)
+	}
+	log.Println("taxlevle", taxLevelindex)
+
+	return taxAmount - income.WHT
 }
 
 type TaxBracket struct {
